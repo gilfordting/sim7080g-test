a9207a009299142ba955ad8311dc6ec66d803d65
Gilford Ting <gilfordting@gmail.com>
2025-03-09 15:19:53 -0400

    working AT commands and HTTP requests

--- src/main.cpp
+++ src/main.cpp
@@ -53,4 +53,99 @@ void loop() {
     while (sim7080g.available()) {
         Serial.write(sim7080g.read());
     }
+    sim7080g.println("AT+CEREG?");
+    Serial.println("sent AT+CEREG? command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+    sim7080g.println("AT+CPIN?");
+    Serial.println("sent AT+CPIN? command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+    sim7080g.println("AT+CNACT?");
+    Serial.println("sent AT+CNACT? command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+    sim7080g.println("AT+CNACT=0,1");
+    Serial.println("sent AT+CNACT command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    sim7080g.println("AT+SHDISC");
+    Serial.println("sent AT+SHDISC command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    sim7080g.println("AT+SHSTATE?");
+    Serial.println("sent AT+SHSTATE? command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    sim7080g.println("AT+SHCONF=\"URL\",\"http://efpi-10.mit.edu\"");
+    Serial.println("sent AT+SHCONF command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+    sim7080g.println("AT+SHCONF=\"BODYLEN\", 1024");
+    Serial.println("sent AT+SHCONF BODYLEN command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    sim7080g.println("AT+SHCONF=\"HEADERLEN\", 350");
+    Serial.println("sent AT+SHCONF HEADERLEN command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    sim7080g.println("AT+SHCONN");
+    Serial.println("sent AT+SHCONN command to sim7080g");
+    delay(5000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    // sim7080g.println("AT+SHREQ=\"example.com\", 1");
+    // Serial.println("sent AT+SHREQ command to sim7080g");
+    // delay(1000);
+    // while (sim7080g.available()) {
+    //     Serial.write(sim7080g.read());
+    // }
+
+    // AT+SHREQ="/request?deviceId=UCFLVA28",3
+
+    sim7080g.println("AT+SHREQ=\"/tunnel_gting/lte_test?num=1\", 2");
+    Serial.println("sent AT+SHREQ command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    sim7080g.println("AT+SHREAD=0, 615");
+    Serial.println("sent AT+SHREAD command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
+
+    sim7080g.println("AT+SHDISC");
+    Serial.println("sent AT+SHDISC command to sim7080g");
+    delay(1000);
+    while (sim7080g.available()) {
+        Serial.write(sim7080g.read());
+    }
 }
